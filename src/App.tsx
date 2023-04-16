c192d1d1eb599b3c8249b770ad3763ebc76cff67
Konrad Kolasa <kolasakonrad1@gmail.com>
2023-04-16 21:25:52 +0200

    feat(project): initial setup for admin site

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => (
+    <div className="App">
+      <h1>
+        Hello
+      </h1>
+    </div>
+  )
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr";
+import eslint from 'vite-plugin-eslint';
+import { fileURLToPath, URL } from "url";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr(), eslint()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+})
